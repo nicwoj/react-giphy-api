d400036d411a982fb6afab2fe35c50834d95107f
Nicole <nwoje13@gmail.com>
2020-06-04 16:43:19 -0400

    added GifCard.js

---
+++ src/components/GifCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class GifCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+
+}
+
+export default GifCard;
