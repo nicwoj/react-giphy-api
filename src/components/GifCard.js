b1c76ab73b2628eb40b34ee358208494d8c922f3
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-05 02:30:48 -0400

    Finished site

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -1,17 +1,10 @@
 import React, { Component } from "react";
 
 class GifCard extends Component {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   constructor(props) {
     super(props);
   }
 
-=======
->>>>>>> fixedSearchFunc
-=======
->>>>>>> 71bbda767e95984fa71a278aa5424c3e21d550ea
   render() {
     let display;
     if (this.props.result) {
@@ -28,63 +21,12 @@ class GifCard extends Component {
       display = <p>Loading...</p>;
     }
     return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <div>
-        
-=======
-
-
-  render() {
-    let display;
-    let sequence = [...Array(this.props.titles.length).keys()];
-    //console.log(sequence);
-
-    if (this.props.titles.length === 0) {
-      display = <p>Loading...</p>;
-    } 
-    else {    
-      const urlList = sequence.map((i) => {
-          return (
-              <li> 
-                  <div>
-                    <img src= {this.props.urls[i]}/><br/>
-                      <h3>{this.props.titles[i]}</h3>
-                  </div>                 
-              </li>
-          )
-      });
-      display = (
-        <>
-          <ul>
-            {urlList}
-          </ul>
-        </>
-      );
-    }
-    return (
-      <div>   
-        {display}
->>>>>>> c850cabcfa13fc718f06ee05b768d3603f106bfa
-      </div>
-=======
-=======
->>>>>>> 71bbda767e95984fa71a278aa5424c3e21d550ea
       <>
         <h1>{this.props.filterBy.toString().toUpperCase()}</h1>
         <div>{display}</div>
       </>
->>>>>>> fixedSearchFunc
     );
   }
 }
 
-<<<<<<< HEAD
 export default GifCard;
-=======
-export default GifCard;
-<<<<<<< HEAD
->>>>>>> c850cabcfa13fc718f06ee05b768d3603f106bfa
-=======
-
->>>>>>> 71bbda767e95984fa71a278aa5424c3e21d550ea
--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,22 +1,10 @@
 import React, { Component } from "react";
 import axios from "axios";
-<<<<<<< HEAD
-<<<<<<< HEAD
-//import GifCard from "./components/GifCard";
-=======
 import GifCard from "./GifCard";
->>>>>>> c850cabcfa13fc718f06ee05b768d3603f106bfa
-=======
-import GifCard from "./GifCard";
->>>>>>> fixedSearchFunc
 
 class SearchField extends Component {
   constructor(props) {
     super(props);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 71bbda767e95984fa71a278aa5424c3e21d550ea
     this.state = {
       search: "",
       result: [],
@@ -112,73 +100,4 @@ class SearchField extends Component {
   }
 }
 
-<<<<<<< HEAD
 export default SearchField;
-=======
-    this.state = { 
-        title: [],
-        GIFurl: [],
-    };
-  }
-
-  componentDidMount() {
-    //const search = this.props.search;
-    const API_KEY = "VDw7hEA1dQuT1rV7K1Nr6l6z8EmwGdCD";
-    //const regSearch = 'http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}';
-    //const Search = 'http://api.giphy.com/v1/gifs/search?q=' + this.props.search + '&api_key=' + API_KEY
-    const trendSearch = 'http://api.giphy.com/v1/gifs/trending?api_key=' + API_KEY;
-    //const randSearch = 'http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}';
-
-
-
-    axios
-      .get(trendSearch)
-      .then((response) => {
-
-        const data = response.data.data;
-        //console.log(data[0]);
-        let titles = [];
-        let GIFsUrl = [];
-        for(let i=0; i<data.length; i++){
-            titles.push(data[i].title);
-            GIFsUrl.push(data[i].images.original.url);
-        }
- 
-        //console.log(titles.length);
-       // console.log(GIFsUrl.length);
-
-
-        this.setState({ 
-            title: titles,
-            GIFurl: GIFsUrl,
-         });
-      })
-      .catch((err) => console.log(err));
-    }
-
-    // inputSearch = (event) => {
-    //   this.setState({ 
-    //     search: event.target.value,
-    //     filterBy: "regular"
-    //   });
-    // }
-
-    render() {
-      
-  
-      return (
-        <>
-        <div className="SearchField">
-
-        <GifCard titles={this.state.title} urls={this.state.GIFurl} />
-        </div>
-        </>
-      );
-    }
-}
-
-export default SearchField;
->>>>>>> c850cabcfa13fc718f06ee05b768d3603f106bfa
-=======
-export default SearchField;
->>>>>>> 71bbda767e95984fa71a278aa5424c3e21d550ea
