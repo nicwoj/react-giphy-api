549ac0b54a3cc2cf77365b69b41b0645a6614550
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-05 02:01:44 -0400

    Fixed search and random button functionality

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 
 class GifCard extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
+    let display;
+    if (this.props.result) {
+      display = this.props.result.map((e) => {
+        return (
+          <>
+            {" "}
+            <h3>{e.title}</h3>
+            <img src={e.imageUrl} alt={e.title} />
+          </>
+        );
+      });
+    } else {
+      display = <p>Loading...</p>;
+    }
     return (
-      <div>
-        
-      </div>
+      <>
+        <h1>{this.props.filterBy.toString().toUpperCase()}</h1>
+        <div>{display}</div>
+      </>
     );
   }
-
 }
 
 export default GifCard;
--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,122 +1,100 @@
 import React, { Component } from "react";
 import axios from "axios";
-//import GifCard from "./components/GifCard";
+import GifCard from "./GifCard";
 
 class SearchField extends Component {
   constructor(props) {
     super(props);
     this.state = {
       search: "",
-      result: {},
+      result: [],
       filterBy: "", //regular, trending, random
-      //gifs: []
     };
   }
 
-  // componentDidMount() {
-  //   //const search = this.props.search;
-  //   const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
-  //   //const regSearch = 'http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}';
-  //   const regSearch =
-  //     "http://api.giphy.com/v1/gifs/search?q=" +
-  //     this.props.search +
-  //     "&api_key=" +
-  //     API_KEY;
-  //   //const trendSearch = 'http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}';
-  //   //const randSearch = 'http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}';
-
-  //   axios
-  //     .get(regSearch)
-  //     .then((response) => {
-  //       const data = response.data;
-
-  //       console.log(data);
-
-  //       const newGif = {
-  //         title: data.data[0].title,
-  //         imageUrl: data.data[0].images.original.url,
-  //       };
-
-  //       this.setState({ search: newGif });
-  //     })
-  //     .catch((err) => console.log(err));
-  // }
-
   getGiphy = () => {
-    const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
-    //const regSearch = 'http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}';
-    const regSearch =
-      "http://api.giphy.com/v1/gifs/search?q=" +
-      this.props.search +
-      "&api_key=" +
-      API_KEY;
-    //const trendSearch = 'http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}';
-    //const randSearch = 'http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}';
-    console.log(regSearch);
-    axios
+    /* Calls the giphy API and sends the correct URL 
+    based on which button the user interacts with*/
+    const API_KEY = process.env.REACT_APP_GIPHY_API_KEY; // API key
+    let regSearch; // URL to send to API
+
+    // Filtering through state.filterBy to send correct URL
+    if (this.state.filterBy === "search") {
+      regSearch = `http://api.giphy.com/v1/gifs/search?q=${this.state.search}&api_key=${API_KEY}`;
+    } else if (this.state.filterBy === "trending") {
+      regSearch = `http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`;
+    } else {
+      regSearch = `http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    }
+
+    axios // Sending request to the API
       .get(regSearch)
       .then((response) => {
-        const data = response.data;
+        let data = response.data;
+        let newGif, currentGif;
+        currentGif = []; // Array to hold the response from API
+        if (this.state.filterBy === "random") {
+          /* The random URL returns one object while the search and 
+          trending return an array of objects, so we push that one object 
+          into an array to keep the response as an array through 
+          the whole program*/
+          currentGif.push(data.data);
+          console.log("ok....", data);
+        } else {
+          // Pushing each object to currentGif array
+          data.data.map((e) => {
+            return currentGif.push(e);
+          });
+        }
 
-        console.log(data);
-        const newGif = data.map((d) => {
+        /* Retreiving title and imageURL from each object 
+        and pushing into a newGif array */
+        newGif = currentGif.map((e) => {
           return {
-            title: d.title,
-            imageUrl: d.images.original.url,
+            title: e.title,
+            imageUrl: e.images.original.url,
           };
         });
 
         this.setState({ result: newGif });
-        console.log(this.state.result);
-
-        /** title: d.title,
-            imageUrl: d.images.original.url, */
       })
       .catch((err) => console.log(err));
   };
 
-  inputSearch = (event) => {
-    this.setState({
-      search: event.target.value,
-      filterBy: "regular",
-    });
-  };
-
+  // Sets the search state to text value
   setSearch = (e) => {
     this.setState({ search: e.target.value });
   };
 
+  // Sets the filterBy state to target value based on which button is clicked
+  setFilterBy = (e) => {
+    this.setState({ filterBy: e.target.value }, this.getGiphy);
+  };
+
   render() {
-    let display;
-    let options = (
-      <>
-        <button>Trending Search</button>
-        <button>Random Search</button>
-      </>
-    );
-
-    if (this.state.search) {
-      display = (
-        <>
-          <img src={this.state.search.imageUrl} alt={this.state.search.title} />
-          <ul>
-            <li>{this.state.search.title} </li>
-          </ul>
-        </>
-      );
-    } else {
-      display = <p>Loading...</p>;
-    }
-
     return (
       <>
         <div>
-          <form>
-            <input type="text" onChange={this.setSearch} />
-            <button onClick={this.getGiphy}>Regular Search</button>
-          </form>
+          <input
+            type="textarea"
+            onChange={this.setSearch}
+            value={this.state.search}
+          />
+          <button onClick={this.setFilterBy} value="search">
+            Regular Search
+          </button>
+          <button onClick={this.setFilterBy} value="trending">
+            Trending Search
+          </button>
+          <button onClick={this.setFilterBy} value="random">
+            Random Search
+          </button>
         </div>
-        <div className="SearchField">{display}</div>
+        <GifCard
+          search={this.state.search}
+          result={this.state.result}
+          filterBy={this.state.filterBy}
+        />
       </>
     );
   }
