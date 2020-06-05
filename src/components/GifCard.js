3903cce76fcdcd4660dae0a97fd0b24a253c21dd
Jiahua Liao <ljh15088131916@gmail.com>
2020-06-05 18:03:51 -0400

    Update

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -14,29 +14,8 @@ class GifCard extends Component {
 
     const API_KEY = process.env.REACT_APP_GIPHY_KEY;
     const trendSearch = `http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`;
-    axios
-      .get(trendSearch)
-      .then((response) => {
-
-        const data = response.data.data;
-        let titles = [];
-        let GIFsUrl = [];
-        for(let i=0; i<data.length; i++){
-            titles.push(data[i].title);
-            GIFsUrl.push(data[i].images.original.url);
-        }
- 
-        console.log(titles.length);
-        console.log(GIFsUrl.length);
-
-
-        this.setState({ 
-            title: titles,
-            GIFurl: GIFsUrl,
-         });
-      })
-      .catch((err) => console.log(err));
-    }
+    this.regORTrendsearch(trendSearch);
+  }
 
   componentDidUpdate(preProps, preState){
     if(preProps.search_method !== this.props.search_method ||
@@ -45,10 +24,9 @@ class GifCard extends Component {
       preProps.search_rank !== this.props.search_rank ||
       preProps.search_tag !== this.props.search_tag){
 
-
+      const API_KEY = process.env.REACT_APP_GIPHY_KEY;
       if(this.props.search_method === 'Trending'){
-          const API_KEY = process.env.REACT_APP_GIPHY_KEY;
-  
+          
           let trendSearch = `http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`;
           if(this.props.search_number !== ''){
             trendSearch = trendSearch.concat(`&limit=${this.props.search_number}`);
@@ -56,109 +34,79 @@ class GifCard extends Component {
           if(this.props.search_rank){
             trendSearch = trendSearch.concat(`&rating=${this.props.search_rank}`);
           }
-
-          console.log(trendSearch);
-          axios
-            .get(trendSearch)
-            .then((response) => {
-
-              const data = response.data.data;
-              //console.log(data[0]);
-              let titles = [];
-              let GIFsUrl = [];
-              for(let i=0; i<data.length; i++){
-                  titles.push(data[i].title);
-                  GIFsUrl.push(data[i].images.original.url);
-              }
-      
-              //console.log(titles.length);
-              //console.log(GIFsUrl.length);
-
-
-              this.setState({ 
-                  title: titles,
-                  GIFurl: GIFsUrl,
-              });
-            })
-            .catch((err) => console.log(err));
+          this.regORTrendsearch(trendSearch);
       }
       else if(this.props.search_method === 'Random'){
-        const API_KEY = process.env.REACT_APP_GIPHY_KEY;
-  
-        let trendSearch = `http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+        let randomSearch = `http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
         if(this.props.search_tag !== ''){
-          trendSearch = trendSearch.concat(`&tag=${this.props.search_tag}`);
+          randomSearch = randomSearch.concat(`&tag=${this.props.search_tag}`);
         }
-        if(this.props.search_rank){
-          trendSearch = trendSearch.concat(`&rating=${this.props.search_rank}`);
+        if(this.props.search_rank !== ''){
+          randomSearch = randomSearch.concat(`&rating=${this.props.search_rank}`);
         }
-
-        console.log(trendSearch);
-        axios
-          .get(trendSearch)
-          .then((response) => {
-
-            const data = response.data.data;
-            //console.log(data[0]);
-            let titles = [], GIFsUrl = [];
-            titles.push(data.title);
-            GIFsUrl.push(data.images.original.url);
-            // for(let i=0; i<data.length; i++){
-            //     titles.push(data[i].title);
-            //     GIFsUrl.push(data[i].images.original.url);
-            // }
-    
-            console.log(titles);
-            console.log(GIFsUrl);
-
-
-            this.setState({ 
-                title: titles,
-                GIFurl: GIFsUrl,
-            });
-          })
-          .catch((err) => console.log(err));
+        this.randomSearch(randomSearch);
       }
       else if(this.props.search_method === "Regular"){
-        const API_KEY = process.env.REACT_APP_GIPHY_KEY;
-  
-        let trendSearch = `http://api.giphy.com/v1/gifs/search?q=${this.props.search_keyword}&api_key=${API_KEY}`;
+        let regularSearch = `http://api.giphy.com/v1/gifs/search?q=${this.props.search_keyword}&api_key=${API_KEY}`;
         if(this.props.search_number !== ''){
-          trendSearch = trendSearch.concat(`&limit=${this.props.search_number}`);
+          regularSearch = regularSearch.concat(`&limit=${this.props.search_number}`);
         }
         if(this.props.search_rank){
-          trendSearch = trendSearch.concat(`&rating=${this.props.search_rank}`);
+          regularSearch = regularSearch.concat(`&rating=${this.props.search_rank}`);
         }
-
-        console.log(trendSearch);
-        axios
-          .get(trendSearch)
-          .then((response) => {
-
-            const data = response.data.data;
-            //console.log(data[0]);
-            let titles = [];
-            let GIFsUrl = [];
-            for(let i=0; i<data.length; i++){
-                titles.push(data[i].title);
-                GIFsUrl.push(data[i].images.original.url);
-            }
-    
-            //console.log(titles.length);
-            //console.log(GIFsUrl.length);
-
-
-            this.setState({ 
-                title: titles,
-                GIFurl: GIFsUrl,
-            });
-          })
-          .catch((err) => console.log(err));
+        this.regORTrendsearch(regularSearch);
       }
         
     }
     
   }
+  regORTrendsearch = (url) => {
+    axios
+    .get(url)
+    .then((response) => {
+
+      const data = response.data.data;
+      let titles = [];
+      let GIFsUrl = [];
+      for(let i=0; i<data.length; i++){
+          titles.push(data[i].title);
+          GIFsUrl.push(data[i].images.original.url);
+      }
+
+      console.log(titles.length);
+      console.log(GIFsUrl.length);
+
+
+      this.setState({ 
+          title: titles,
+          GIFurl: GIFsUrl,
+       });
+    })
+    .catch((err) => console.log(err));
+  };
+
+  randomSearch = (url) => {
+
+    axios
+      .get(url)
+      .then((response) => {
+        const data = response.data.data;
+        let titles = [], GIFsUrl = [];
+        titles.push(data.title);
+        GIFsUrl.push(data.images.original.url);
+
+    
+        console.log(titles);
+        console.log(GIFsUrl);
+
+
+        this.setState({ 
+            title: titles,
+            GIFurl: GIFsUrl,
+        });
+      })
+     .catch((err) => console.log(err));
+  };
 
   render() {
     let display;
@@ -173,7 +121,7 @@ class GifCard extends Component {
           return (
               <li> 
                   <div>
-                    <img src= {this.state.GIFurl[i]}/><br/>
+                    <img src= {this.state.GIFurl[i]} alt={this.state.title[i]}/><br/>
                       <h3>{this.state.title[i]}</h3>
                   </div>                 
               </li>
